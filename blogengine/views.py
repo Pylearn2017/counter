a08b9119f5d148ca4962be920bc17ceec2251b6e
Pylearn2017 <alexlik73@gmail.com>
2019-04-03 17:23:09 +0300

    add txt
    Add new file.txt

--- blogengine/views.py
+++ blogengine/views.py
@@ -1,8 +1,30 @@
 from django.http import HttpResponse
 
 def hello(request):
-	global count
+	global count, color, flag
+	if flag:
+		if color <= 255:
+			color += 1
+		else:
+			flag = False
+	else: 	
+		if color >= 0:
+			color -= 1
+		else:
+			flag = True		
 	count += 1
-	return HttpResponse(f'<h1 align="center">You are visitor number {count} while the server is running</h1>')
+	return HttpResponse(
+	f'<div style="\
+		background-color:rgb({color},{color},{color}); \
+		width: 100%; \
+		height: 100%"> \
+			<h1 style="\
+			color:rgb({255-color},{255-color},{255-color};"\
+			align= "center">\
+				You are visitor number {count} while the server is running\
+			</h1>\
+	</div>')
 
-count = 0	
+count = 0
+color = 255	
+flag = True
