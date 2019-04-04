a641cf1611f7af443523bae4312301ecd3645178
Pylearn2017 <alexlik73@gmail.com>
2019-04-04 19:03:23 +0300

    +5

--- blogengine/views.py
+++ blogengine/views.py
@@ -4,23 +4,20 @@ def hello(request):
 	global count, color, flag
 	if flag:
 		if color <= 255:
-			color += 1
+			color += 5
 		else:
 			flag = False
 	else: 	
 		if color >= 0:
-			color -= 1
+			color -= 5
 		else:
 			flag = True		
 	count += 1
 	return HttpResponse(
-	f'<div style="\
-		background-color:rgb({color},{color},{color}); \
+	f'<div style="background-color:rgb({color},{color},{color}); \
 		width: 100%; \
 		height: 100%"> \
-			<h1 style="\
-			color:rgb({255-color},{255-color},{255-color};"\
-			align= "center">\
+			<h1 style="color:rgb({255-color},{255-color},{255-color});" align= "center">\
 				You are visitor number {count} while the server is running\
 			</h1>\
 	</div>')
