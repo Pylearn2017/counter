04377d92fc494d96485acfe93055457ce334b464
Pylearn2017 <alexlik73@gmail.com>
2019-04-05 19:27:49 +0300

    weed duty

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from time import strftime
+
+def week_duty():
+	number_of_week = strftime('%W')
+	names_list = ['Александр', 'Динар', 'Ренат', 'Николай', 'Ольга',]
+	number_of_week = int(number_of_week)
+	i = number_of_week % len(names_list)
+	name = names_list[i]
+	return name
+
+
+def data():
+	day = strftime('%d.%m.%Y')
+	return day
 
 # Create your views here.
+def posts_list(request):
+	name = week_duty().upper()
+	day = data()
+	return render(request, 'blog/index.html', context = {'name' : name, 'data' : day})
+
+data()
